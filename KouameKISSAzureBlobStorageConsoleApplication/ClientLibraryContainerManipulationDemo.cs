b8360af38f4abda73a0d31750d7e28b50e89ba5f
Marcel Kouame <m@kouamekiss.com>
2018-02-25 17:29:03 -0500

    updates

---
+++ KouameKISSAzureBlobStorageConsoleApplication/ClientLibraryContainerManipulationDemo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace KouameKISSAzureBlobStorageConsoleApplication
+{
+    class ClientLibraryContainerManipulationDemo
+    {
+        public static string StorageConnection { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var StorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnection"));
+
+            var blobClient = StorageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("HelloWorld");
+
+            container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+
+            Console.ReadKey();
+        }
+    }
+}
